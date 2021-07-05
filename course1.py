643c3a29255c44a749d6c3b55f38a410dabc9051
Gökhan Taşkın <gkhantaskin34@gmail.com>
2021-07-05 18:26:40 +0300

    project 1 added

---
+++ course1.py
@@ -0,0 +1,39 @@
+# variable types
+
+#string (str)
+name = "Gokalp"
+number = str(15)
+number = "15"
+
+#integer (int)
+age = 15
+age = int("15")
+
+#bool (bool)
+isTall = True
+isShort = False
+
+
+
+
+
+
+#input and output type
+
+#printing something print()
+print("This is an output")
+
+#input something
+age = input("Type Your Age :")
+print("You are "+ str(age))
+
+
+
+
+
+
+#loops
+#for loops
+for i in ["Gökalp", "Gökhan", "Ömer", "Asya", "Mia"]:
+    print(i)
+
---
+++ project1.py
@@ -0,0 +1,6 @@
+#program starts
+#program asks clients name
+#program asks clients surname
+#program asks clients age
+#program prints clients name, surname, age
+#program prints age + 10
