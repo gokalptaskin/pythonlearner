c6c307ca5167d60c7b325f86edbfdc3d9a949236
Gökhan Taşkın <gkhantaskin34@gmail.com>
2021-07-06 00:17:49 +0300

    project 2 added

---
+++ project3.py
@@ -0,0 +1,15 @@
+##### project 2 ########
+# rectangle printer #
+
+
+# program starts
+# program asks client for row size
+# program asks client for column size
+# prints rectangle with "*" by row and column size
+
+
+
+
+
+
+
